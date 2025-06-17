88be4fd97f5d49581005a1bebc78c629f8924aa2
tauqirsarwar1 <tauqirsarwar1@users.noreply.github.com>
2025-06-17 11:08:52 +0000

    deploy: 5dd6c6c6ca9f19cc4f14a93d59cc840d82d9178e

--- local_run_web.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Run API tests
-python -m pytest -vv -s  --gherkin-terminal-reporter --driver=Chrome  --html="./output_data/reports/" --self-contained-html  --capability headless True  --reruns 1 --reruns-delay 2 --tags="web_tests" -n 2
