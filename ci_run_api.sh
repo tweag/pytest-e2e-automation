f41eda6e948f7063137fe40ddc47586441f28ca0
tauqirsarwar1 <tauqirsarwar1@users.noreply.github.com>
2025-06-17 09:41:46 +0000

    deploy: eb543080d8131f0fbdf60620f1ac374bac56b6e1

--- ci_run_api.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Run API tests
-python -m pytest -vv -s --html="./output_data/reports/" --tags="$1" --self-contained-html --reruns 1 --reruns-delay 2 --alluredir=allure-results \
---slack-webhook-url=$2 --slack-channel=pytest-test-automation --slack-results-url=https://tweag.github.io/pytest-e2e-automation --teams-webhook-url=$3 \
---teams-results-url=https://tweag.github.io/pytest-e2e-automation --slack-failure-only=true --teams-failure-only=true -n=4
