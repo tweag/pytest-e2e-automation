1aca83b4ba80f14fbe9f749ccf10919894a67ebb
tauqirsarwar1 <tauqirsarwar1@users.noreply.github.com>
2025-06-17 10:43:39 +0000

    deploy: 614ad4a7d04bc90edb6d3b2a06d1e2a80a609d8e

--- local_run_android.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# Run API tests
-python -m pytest -vv -s  --disable-warnings --gherkin-terminal-reporter --driver=Appium  --html="./output_data/reports/" --self-contained-html  \
---variables="env_configs/android_mobile_docker.json"  --reruns 1 --reruns-delay 2 --tags="android_mobile_tests"
